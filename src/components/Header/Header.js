0af63181bb4e7a8c218207f99e000491f22d5996
Corey Mcallen <mc-corey@hotmail.com>
2019-01-09 19:50:07 +0000

    Add existing file

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,7 @@ return(
     <div  >
     <img className=" mw5 black center flex flex-column  "src={dice} alt="loading..." />
      <h2 className=" f2 tc pb col "> Let the Dice Decide</h2>  
-        <h3 className="  mw6 center f4 tc pb pb3 poo " > Bored of having to think about your life choices? Well thanks to Dice Decide you don’t have to, simply add the decisions you’re wrestling with into the different boxes, hit roll and let the Dice Decide.</h3>                                         
+        <h3 className="  mw6 center f4 tc pb pb3 poo " > Bored of making your own decisions? <br/> Well thanks to Dice Decide you don’t have to, now you can simply add the decisions you’re wrestling with into the different boxes, hit roll and let the Dice Decide.</h3>                                         
    </div>
     );
 }
